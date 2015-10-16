b8906c340340c510fda3feece6013c27e1421ef9
shivam kaushik <shivamkaushik@shivams-macbook-air.local>
2015-10-16 22:50:42 +0530

    Initial Commit

---
+++ myLittleMonster/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  myLittleMonster
+//
+//  Created by shivam kaushik on 16/10/15.
+//  Copyright © 2015 shivam kaushik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view, typically from a nib.
+  }
+
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+    // Dispose of any resources that can be recreated.
+  }
+
+
+}
+
